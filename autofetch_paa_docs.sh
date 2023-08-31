5c2f567990ab86e34cb682688d52974326876d9d
lcf <kevinfinley@163.com>
2023-08-31 13:27:37 +0800

    added autofetch script

---
+++ autofetch_paa_docs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# Set the repository URL
+repo_url=git@github.com:Peiyang-Aeromodelling-Association/documentation-deploy.git
+
+# Set the local destination for the repository
+local_destination=/home/lcf/paa_docs
+
+# Clone the repository if it doesn't already exist
+if [ ! -d "$local_destination" ]; then
+  git clone --recursive $repo_url $local_destination
+fi
+
+# Navigate to the local repository directory
+cd $local_destination
+
+git pull --recurse-submodules
+git submodule foreach git pull --force origin gh-pages:gh-pages
