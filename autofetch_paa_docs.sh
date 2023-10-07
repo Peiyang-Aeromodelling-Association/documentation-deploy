caca30c3b526bb8b4b91086ae5e3b1c44372cbfb
lcf <kevinfinley@163.com>
2023-10-07 14:49:38 +0800

    updated autofetch script to use key_dir

--- autofetch_paa_docs.sh
+++ autofetch_paa_docs.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/bash
 
+# read key_dir from 1st argument
+key_dir=$1
+
+# function load_key: load ssh key from key_dir via repo name, return content of the key
+function load_key {
+  key_name=$1
+  key_path=$key_dir/$key_name
+  if [ -f "$key_path" ]; then
+    cat $key_path
+  else
+    echo "Key $key_name not found in $key_dir"
+    exit 1
+  fi
+}
+
 # Set the repository URL
 repo_url=git@github.com:Peiyang-Aeromodelling-Association/documentation-deploy.git
 
@@ -8,11 +23,29 @@ local_destination=~/documentation-deploy
 
 # Clone the repository if it doesn't already exist
 if [ ! -d "$local_destination" ]; then
-  git clone --recursive $repo_url $local_destination
+  GIT_SSH_COMMAND="ssh -i $(load_key documentation-deploy)" git clone $repo_url $local_destination
 fi
 
 # Navigate to the local repository directory
 cd $local_destination
 
-git pull --recurse-submodules
-git submodule foreach git pull --force origin gh-pages:gh-pages
+# git pull documentation-deploy
+GIT_SSH_COMMAND="ssh -i $(load_key documentation-deploy)" git pull origin main:main
+
+function pull_submodule {
+  submodule_name=$1
+  submodule_path=$local_destination/$submodule_name
+  if [ -d "$submodule_path" ]; then
+    echo "Pulling $submodule_name"
+    GIT_SSH_COMMAND="ssh -i $(load_key $submodule_name)" git pull origin gh-pages:gh-pages
+  else
+    echo "Submodule $submodule_name not found in $local_destination"
+    exit 1
+  fi
+}
+
+# list all submodules, for each submodule, set ssh key and pull, ssh key is the repo name
+submodule_names=$(git submodule | awk '{print $2}')
+for submodule_name in $submodule_names; do
+  pull_submodule $submodule_name
+done
